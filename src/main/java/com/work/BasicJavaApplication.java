4d48f80345dcd30cae34ffe59e530bb5dcedb9ad
Odin <aaa@163.com>
2021-01-20 10:22:47 +0800

    basic-java Java基础学习

---
+++ src/main/java/com/work/BasicJavaApplication.java
@@ -0,0 +1,13 @@
+package com.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJavaApplication.class, args);
+    }
+
+}
